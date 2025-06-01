2b9616bbb8b7d5709ee0c75324529de5e970bf45
Wesley0010012 <wglaurindo33@gmail.com>
2025-06-01 11:54:45 -0300

    feat: implementa método para busca por usuário

--- src/main/java/br/com/fatecmogidascruzes/pizzaria_mario/repository/UsuarioRepository.java
+++ src/main/java/br/com/fatecmogidascruzes/pizzaria_mario/repository/UsuarioRepository.java
@@ -1,10 +1,12 @@
 package br.com.fatecmogidascruzes.pizzaria_mario.repository;
 
-import br.com.fatecmogidascruzes.pizzaria_mario.model.Usuario;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import br.com.fatecmogidascruzes.pizzaria_mario.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByUsernameAndPassword(String username, String password);
+    Optional<Usuario> findByUsername(String username);
 }
